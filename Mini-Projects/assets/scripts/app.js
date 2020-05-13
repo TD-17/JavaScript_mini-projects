7190fd021dd9eb437cbf7591a3e791804097f940
tanya <shanya.2018td@gmail.com>
2020-05-13 14:38:11 +0530

    used classes

--- Mini-Projects/assets/scripts/app.js
+++ Mini-Projects/assets/scripts/app.js
@@ -2,48 +2,76 @@ class Product{
     title='DEFAULT';
     imageUrl;
     price;
-    description;    
+    description; 
+    
+    constructor(title, image, desc, price){
+        this.title=title;
+        this.imageUrl=image;
+        this.description=desc;
+        this.price=price;
+    }
 }
 
-const productList={
-    products:[
-        {
-            title:'A Pillow',
-            imageUrl:
-            'https://cdn.shopify.com/s/files/1/0178/0240/products/non_adjustable1_600x.jpg?v=1553174661',
-            price: 19.99,
-            description: 'A soft pillow!'
+class ProductItem{
+    constructor(product){
+        this.product=product;
+    }
 
-        },
-        {
-            title: 'A carpet',
-            imageUrl:
+    addToCart()
+    {
+        console.log('Adding product to cart.....');
+        console.log(this.product);
+    }
+    render(){
+        const prodEl=document.createElement('li');
+            prodEl.className='product-item';
+            prodEl.innerHTML=`
+            <div>
+             <img src="${this.product.imageUrl}" alt="${this.product.title}">
+             <div class="product-item__content">
+               <h2>${this.product.title}</h2>
+               <h3>\$${this.product.price}</h3>
+               <p>${this.product.description}</p>
+               <button>Add to Cart</button>
+             </div>
+            </div>
+            `;
+            const addCartButton=prodEl.querySelector('button');
+            addCartButton.addEventListener('click',this.addToCart.bind(this));
+        return prodEl;
+    }
+}
+
+class ProductList{
+    products=[
+        new Product(
+            'A Pillow',
+            'https://cdn.shopify.com/s/files/1/0178/0240/products/non_adjustable1_600x.jpg?v=1553174661',
+            'A soft pillow!',
+            19.99
+
+        ),
+        new Product(
+            'A carpet',
             'https://images-na.ssl-images-amazon.com/images/I/81KSEQwRa9L._SL1200_.jpg',
-            price: 89.99,
-            description: 'A carpet which you might like or not!'
-        }
-    ],
+            'A carpet which you might like or not!',
+            89.99
+        )
+        
+    ];
+    constructor() {}
     render(){
         const renderHook=document.getElementById('app');
         const prodList=document.createElement('ul');
         prodList.className='product-list';
         for(const prod of this.products){
-            const prodEl=document.createElement('li');
-            prodEl.className='product-item';
-            prodEl.innerHTML=`
-            <div>
-             <img src="${prod.imageUrl}" alt="${prod.title}">
-             <div class="product-item__content">
-               <h2>${prod.title}</h2>
-               <h3>\$${prod.price}</h3>
-               <p>${prod.description}</p>
-             </div>
-            </div>
-            `;
+            const productItem=new ProductItem(prod);
+            const prodEl=productItem.render();
             prodList.append(prodEl);
         }
         renderHook.append(prodList);
     }
 }
 
+const productList=new ProductList();
 productList.render();
