51bd84e28668e08c4d0ad8a6aa707bceb4d2b202
tanya <shanya.2018td@gmail.com>
2020-06-03 01:17:59 +0530

    saving data to local storage

--- Movie-seat-booking/script.js
+++ Movie-seat-booking/script.js
@@ -5,19 +5,30 @@ const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
 let ticketPrice = +movieSelect.value;
+
+// Save selected movie index and price
+function setMovieData(movieIndex, moviePrice)
+{
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
  
 // update total and count
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
     const selectedSeatsCount = selectedSeats.length;
 
-    count.ineerText = selectedSeatsCount;
+    count.innerText = selectedSeatsCount;
     total.innerText = selectedSeatsCount * ticketPrice;
 }
 
 // movie select event
 movieSelect.addEventListener('change', e => {
     ticketPrice = +e.target.value;
+    setMovieData(e.target.selectedIndex, e.target.value);
     updateSelectedCount();
 })
 
