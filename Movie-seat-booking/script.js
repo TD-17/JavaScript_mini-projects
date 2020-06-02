e49338afaa76d14364b0ca9a2739cc50bdb65ce0
tanya <shanya.2018td@gmail.com>
2020-06-03 02:22:23 +0530

    populating UI with saved data

--- Movie-seat-booking/script.js
+++ Movie-seat-booking/script.js
@@ -3,6 +3,7 @@ const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
+populateUI();
 
 let ticketPrice = +movieSelect.value;
 
@@ -25,6 +26,26 @@ function updateSelectedCount() {
     total.innerText = selectedSeatsCount * ticketPrice;
 }
 
+// get data from localStorage and populate UI
+function populateUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+    if(selectedSeats!==null && selectedSeats.length>0)
+    {
+        seats.forEach((seat, index) => {
+            if(selectedSeats.indexOf(index) > -1) {
+                seat.classList.add('selected');
+            }
+        });
+    }
+
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+    if(selectedMovieIndex !== null) {
+        movieSelect.selectedIndex = selectedMovieIndex;
+    }
+}
+
 // movie select event
 movieSelect.addEventListener('change', e => {
     ticketPrice = +e.target.value;
@@ -40,3 +61,6 @@ container.addEventListener('click', e=> {
     }
    
 })
+
+// initial count and total set 
+updateSelectedCount();
