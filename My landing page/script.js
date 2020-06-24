9fd0b4d9ce0f15ae2b8b758514c9bb567ee9ebf8
tanya <shanya.2018td@gmail.com>
2020-06-24 23:36:12 +0530

    completed style.css file and started script.js file

---
+++ My landing page/script.js
@@ -0,0 +1,4 @@
+const toggle = document.getElementById('toggle');
+const close = document.getElementById( 'close');
+const open = document.getElementById('open');
+const modal = document.getElementById('modal');
